0df0330a5adeae2b1607d53075b0ceaa946166b3
sanguyen231198 <72490466+sanguyen231198@users.noreply.github.com>
2023-06-29 11:10:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log('Hello World');
+[
+  {
+    "operation": "shift",
+    "spec": {
+      "date": "date",
+      "resourcePermissions": {
+        "type": "resourcePermissions.type",
+        "header": {
+          ":authority": "resourcePermissions.header.authority",
+          ":path": "resourcePermissions.header.path",
+          ":method": "resourcePermissions.header.method",
+          "cache-control": "resourcePermissions.header.cache_control",
+          "x-resuest-id": "resourcePermissions.header.x_request_id"
+        }
+      }
+    }
+  },
+  {
+    "operation": "modify-overwrite-beta",
+    "spec": {
+      "*": "=replaceAll",
+      "*": {
+        ":": "",
+        "-": "_"
+      }
+    }
+  },
+  {
+    "operation": "remove",
+    "spec": {
+      "resourcePermissions": {
+        ":": "",
+        "-": ""
+      }
+    }
+  }
+]
